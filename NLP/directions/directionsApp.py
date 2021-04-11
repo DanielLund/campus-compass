4e0e49ce8f37283b10df36e8083e7a5e8f473ab8
John_L_Smith <68287860+johnlsmith99@users.noreply.github.com>
2021-04-11 05:01:24 +0100

    Add files via upload

---
+++ NLP/directions/directionsApp.py
@@ -0,0 +1,26 @@
+import pyowm
+from XXX import XXX
+from config.config_reader import ConfigReader
+
+class directionInformation():
+    def __init__(self):
+        self.config_reader = ConfigReader()
+        self.configuration = self.config_reader.read_config()
+        """self.owmapikey = self.configuration['A*_SEARCH_API_KEY'] 
+        self.owm = pyowm.OWM(self.owmapikey)
+
+    def get_direction_info(self,city):
+
+        self.city=city
+
+
+
+
+
+
+
+
+
+
+        self.bot_says = ###STRINGS OF DIRECTIONS ADDED TOGETHER (defined in function above)###"""
+        return self.bot_says
